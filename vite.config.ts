65f4fee4bcdd34ec3402d8319985990d85c1bc72
blueblue2x <dariantely@gmail.com>
2025-05-16 00:15:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -23,8 +23,9 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
-  build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
+ build: {
+  outDir: "dist", // inside client
+  emptyOutDir: true,
+} 
   },
 });
